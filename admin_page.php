bc61f99c29d025d41c8617604060159dce117c9c
H1325 <59647908+H1325@users.noreply.github.com>
2023-11-26 22:12:18 +0530

    Add files via upload

---
+++ admin_page.php
@@ -0,0 +1,170 @@
+<?php
+
+@include 'config.php';
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>gaming</title>
+    <link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<section class="header">
+    <div class="container">
+        <img src="ezgif.com-crop (1).gif">
+        <o>Games Street</o>
+        
+        <a href="login_form.php" class="btn">Login</a>
+        
+    </div>
+    
+<h1>Just One More Game....</h1>
+<p>To Win You've Got To Stay In The Game</p>
+    </div>
+</section>  
+<section class="gallery">
+<h1>Gaming Gallery</h1>
+<div class="container">
+<div class="row">
+<div class="col-md-3">
+    <div href="login_form.php" class="gallery-box">
+        <a href="login_form.php"><img src="ball.png" ></a>
+        <h7>Ball Run</h7>  
+    </div>  
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="alice.png" ></a>
+        <h7> Wonder Land</h7>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="R.jpg" ></a>
+        <h7>Game 3</h7>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="R.jpg" ></a>
+        <h7>Game 4</h7>
+    </div>    
+</div> 
+</div> 
+</div>
+</section>
+<div class="container">
+<div class="row">
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="R.jpg" ></a>
+        <h7>Game 5</h7>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="R.jpg" ></a>
+        <h7>Game 6</h7>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="R.jpg" ></a>
+        <h7>Game 7</h7>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <a href="login_form.php"><img src="R.jpg" ></a>
+        <h7>Game 8</h7>
+    </div>    
+</div> 
+</div> 
+</div>
+</section>
+<section class="banner">
+    <div class="banner-highlighths">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                <h2>Failure doesn't mean Game Over,It means Try Again with Experience</h2>
+                </div>
+                <div class="col-md-4">
+                    <a type="button" href="register_form.php" class="download-btn">Register</a>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>  
+<section class="users-feedback">
+<h1>Contributers</h1>
+<div class="container">
+<div class="row">
+<div class="col-md-4">
+    <div class="user-review">
+    <p>As the developer of the game , i would like to say ....many people won't like 2D games.We will try to improve our games..</p>
+    <h5>Shivang</h5>
+    <small>Game Designer</small>
+    </div> 
+    <img src="shiv.jpg">
+</div>
+<div class="col-md-4">
+    <div class="user-review">
+    <p>We will try to enhance the design of the website according to the developing standarards.</p>
+    <h5>Sravani</h5>
+    <small>Web Developer</small>
+    </div> 
+    <img src="sree.jpg">
+</div>
+<div class="col-md-4">
+    <div class="user-review">
+    <p>Kindly be patient until we solve the bugs....and we will do necessaary testings in respective time periods.</p>
+    <h5>Suraksha</h5>
+    <small>Technical Writer</small>
+    </div> 
+    <img src="sur.jpg">
+</div>
+</div>    
+</div>
+</section>
+<section class="footer">
+<div class="container">
+<div class="row">
+    <div class="col-md-3">
+    <img src="ezgif.com-crop (1).gif" class="footer-logo">
+    <p>Games Street has the best free games section and offers the most fun experience to play alone.Our games are playable on desktop,tablet and mobile so you can enjoy them at home or on the road.</p>
+    </div>
+    <div class="col-md-3">
+    <h4>About Us</h4>
+    <p>Privacy Policy</p>
+    <p>Cookies Policy</p>
+    <p>Help</p>
+    <p>FAQs</p>
+    </div>
+    <div class="col-md-3">
+    <h4>Quick Contacts</h4>
+    <p><i class="fa fa-phone-square"></i>+91 9997486269</p>
+    <p><i class="fa fa-envelope"></i>sravani060402@gamil.com</p>
+    <p><i class="fa fa-home"></i>HNBGU Chauras campus,Srinagar,Uttarakhand-246174</p>
+    </div>
+    <div class="col-md-3">
+    <h4>Follow us on</h4>
+    <p><i class="fa fa-facebook-official"></i>Facebook</p>
+    <p><i class="fa fa-youtube-play"></i>YouTube</p>
+    <p><i class="fa fa-twitter"></i>Twitter</p>
+    <p><i class="fa fa-instagram"></i>Instagram</p>
+    </div>
+</div><hr> 
+    <p class="copyright">Made by<i class="fa fa-book"></i>IT 2k19-23 Batch</p>
+</div>    
+</section>
+    <script src="script.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
---
+++ user_page.php
@@ -0,0 +1,61 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>User Page</title>
+    <link rel="stylesheet" href="css/style1.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet"href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+</head>
+<body>
+<section class="header">
+    <div class="container">
+        <o>Games Street</o>
+    </div>      
+</section>  
+<section class="gallery">
+<h1>Gaming Gallery</h1>
+<div class="container">
+<div class="row">
+<div class="col-md-3">
+    <div class="gallery-box">
+        <img src="ball.png" ></a>
+        <div class="game">
+        <h2>Ball Run</h2>
+        <a href="game1.php">Play </a>
+    </div> 
+    </div>  
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <img src="alice.png" ></a>
+    <div class="game">
+    <h2>Wonder Land</h2>
+    <a href="game2.php">Play </a>
+    </div>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <img src="R.jpg" ></a>
+    <div class="game">
+        <h2>Game 2</h2>
+        <a href="Game_1.exe">Play </a>
+    </div>
+    </div>    
+</div>
+<div class="col-md-3">
+    <div class="gallery-box">
+    <img src="R.jpg" ></a>
+    <div class="game">
+        <h2>Game 1</h2>
+        <a href="C:\Users\srava\OneDrive\Desktop\MY Project\game.exe">Play </a>
+    </div>
+    </div>    
+</div> 
+</div> 
+</div>
+</section>
+</body>
+</html>
